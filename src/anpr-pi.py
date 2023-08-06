62d0f11155d8f12bc809e22ab3b136645632c4df
cloudfright <ben.dyer@hey.com>
2023-08-06 14:01:16 +0100

    First steps in testing Coral TPU

--- src/anpr-pi.py
+++ src/anpr-pi.py
@@ -2,9 +2,9 @@ import cv2, time
 from collections import deque
 import logging
 from multiprocessing import Process, Manager, Queue
-from detect import ObjectDetector
+from detect_tpu import TpuObjectDetector
 from multiprocessing.pool import ThreadPool
-# from platereader import PlateReader
+from platereader import PlateReader
 from picamera2 import Picamera2
 
 import datetime
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     # create a thread pool to do the ocr processing (leave somme headroom)
     pool = ThreadPool(processes = int(cv2.getNumberOfCPUs() / 2 ))
 
-    detector = ObjectDetector()
-    # plate_reader = PlateReader()
+    detector = TpuObjectDetector()
+    plate_reader = PlateReader()
 
     try:
         # start the video stream
@@ -42,21 +42,21 @@ if __name__ == '__main__':
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             filename = detector.detect_objects(frame)
 
-            # if filename:
-            #     # motion detected 
-            #     movement_count += 1
-            #     ocr_queue.append(filename)
-            # else:
-            #     if (movement_count > 0):
-            #          movement_count -= 1
-            #     else:    
-            #         # motion ended                   
-            #         if len(ocr_queue):
-            #             logging.debug("motion ended")
-            #             ocr_list_to_process = []
-            #             while (len(ocr_queue)):
-            #                 ocr_list_to_process.append(ocr_queue.popleft())
-            #                 result = pool.apply_async(plate_reader.read_plate, args=([ocr_list_to_process]))
+            if filename:
+                # motion detected 
+                movement_count += 1
+                ocr_queue.append(filename)
+            else:
+                if (movement_count > 0):
+                     movement_count -= 1
+                else:    
+                    # motion ended                   
+                    if len(ocr_queue):
+                        logging.debug("motion ended")
+                        ocr_list_to_process = []
+                        while (len(ocr_queue)):
+                            ocr_list_to_process.append(ocr_queue.popleft())
+                            result = pool.apply_async(plate_reader.read_plate, args=([ocr_list_to_process]))
    
 
             new_frame_time = time.time()
@@ -65,7 +65,7 @@ if __name__ == '__main__':
             fps = int(fps)  
             fps = str(fps)
   
-            print(fps)
+            # print(fps)
                 # cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
                 # cv2.imshow('frame', frame)
 
---
+++ src/detect_tpu.py
@@ -0,0 +1,76 @@
+
+from datetime import datetime
+import cv2
+import numpy as np
+import logging
+from pycoral.adapters import common
+from pycoral.adapters import detect
+from pycoral.utils import edgetpu
+from pycoral.utils import dataset
+
+
+class TpuObjectDetector(object):
+
+    DETECT_THRESHOLD = 0.5
+
+    def __init__(self):
+    
+        self.interpreter = edgetpu.make_interpreter('./data/model/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite')
+        self.interpreter.allocate_tensors()
+        self.labels = dataset.read_label_file('./data/model/coco_labels.txt') 
+        logging.info("TpuObjectDetector initialized")
+
+    def detect_objects(self,frame): 
+
+        # if self.image_acc is None:
+        #     self.image_acc = np.empty(np.shape(frame))
+        
+        # Compute difference.
+        # image_diff = cv2.absdiff(self.image_acc.astype(frame.dtype),frame)
+
+        input_frame = cv2.resize(frame, common.input_size(self.interpreter))
+
+        common.set_input(self.interpreter, input_frame)
+        self.interpreter.invoke()
+        objects = detect.get_objects(self.interpreter, score_threshold=TpuObjectDetector.DETECT_THRESHOLD)
+        for obj in objects:
+            bbox = [obj.bbox.xmin, obj.bbox.ymin, obj.bbox.xmax, obj.bbox.ymax]
+            classId = int(obj.id)
+            label = self.labels.get(classId, ["unknown"])
+            logging.debug("Object detected: %s, %s %f" % (label, bbox, obj.score))
+
+
+        # bbox=list(bbox)
+        # confs= list(np.array(confs).reshape(1,-1)[0])
+        # confs=list(map(float,confs))  # class 'float'
+
+        # # non max suppression
+        # indices=cv2.dnn.NMSBoxes(bbox,confs,self.thres,self.nms_threshold)
+
+        #  # Accumulate.
+        # cv2.accumulateWeighted(frame,self.image_acc,self.alpha)
+
+        # filename = None
+        # for i in indices:
+        #     box=bbox[i]
+        #     x,y,w,h = box[0],box[1],box[2],box[3]
+            
+        #     objClass = self.classNames[classIds[i] - 1]
+        #     text = "{}: {:.4f}".format(objClass, confs[i]) 
+
+        #     # cv2.rectangle(frame,(x,y),(x+w,h+y), color=(0, 255, 0), thickness=1)
+        #     # cv2.putText(frame, text, (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
+            
+        #     # Calculate the average pixel value or mean intensity
+        #     roi = image_diff[y:y+h,x:x+w]
+        #     average_brightness = cv2.mean(roi)[0]
+
+        #     if (average_brightness > 10 and objClass in self.objectsToDetect):
+        #         now = datetime.now()
+        #         object_frame = frame[y:y+h,x:x+w]
+        #         filename = "./images/capture/%s.jpg" % now.strftime("%Y-%m-%d-%H-%M-%S-%f")
+        #         cv2.imwrite(filename, object_frame)
+        #         # logging.debug("Movement: %s, Index: %d, class %s, brightness %d" % (datetime.now(), i, text, average_brightness))
+
+        # return filename
+    
